5fcd505f8c5cc24cf4b64c6b122a10d3410780ab
Maxime <maxime.brignon@eliberty.fr>
2024-08-05 16:50:03 +0200

    chore: require sf4+ and fix tree builder config

--- sf2-bundle/PUGX/GodfatherBundle/DependencyInjection/Configuration.php
+++ sf2-bundle/PUGX/GodfatherBundle/DependencyInjection/Configuration.php
@@ -13,8 +13,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('godfather');
+        $treeBuilder = new TreeBuilder('godfather');
+        $rootNode = $treeBuilder->getRootNode();
 
         $this->addContextSection($rootNode);
 
